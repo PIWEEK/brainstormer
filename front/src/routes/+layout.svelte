9f339ccf08c62d580acaa8946de1997dd9ac8eb6
andy <andres.gonzalez79@gmail.com>
2023-07-18 12:06:49 +0200

    tokenssss

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 
  import "../styles/normalize.css";
  import "../styles/fonts.css";
+ import "../styles/tokens.css";
  import "../styles/global.css";
 
  import Header from "$components/Header.svelte";
